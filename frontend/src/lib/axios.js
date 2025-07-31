ddbebaf4123463acb4a16ca4e8bed561edb1a733
vichu6382 <vishnumathappan16@gmail.com>
2025-07-31 13:28:33 +0530

    31/07/25 update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BACKEND_URL,
   withCredentials: true,
 });
